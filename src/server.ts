9036a7ff97622edb8109efa7a28642909cdd9959
Jonas <jonasdalethese1@gmail.com>
2023-08-19 13:54:59 -0300

    Registro de usuários

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { router } from "./routes";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(express.static("public"));
 app.use(adminJs.options.rootPath, adminJsRouter);
 app.use(router);
