ac09ad6a0cece6979594b0dd0960879260fa649a
Jonas <jonasdalethese1@gmail.com>
2023-08-21 18:08:52 -0300

    reconhecendo likes e favorites

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import { courseService } from "../services/courseService";
 import { error } from "console";
 import { getPaginationParams } from "../helpers/getPaginationParams";
+import { AuthenticatedRequest } from "../middlewares/auth";
+import { likeService } from "../services/likeService";
+import { favoriteService } from "../services/favoriteService";
 
 export const coursesController = {
   /** GET /courses/featured */
@@ -32,8 +35,7 @@ export const coursesController = {
     const [page, perPage] = getPaginationParams(req.query);
 
     try {
-      if (typeof name !== "string")
-        throw new Error("name param must be type string");
+      if (typeof name !== "string") throw new Error("name param must be type string");
 
       const courses = await courseService.findByName(name, page, perPage);
       return res.json(courses);
@@ -44,12 +46,20 @@ export const coursesController = {
     }
   },
   /** GET /courses/:id */
-  show: async (req: Request, res: Response) => {
-    const { id } = req.params;
+  show: async (req: AuthenticatedRequest, res: Response) => {
+    const userId = req.user!.id;
+    const courseId = req.params.id;
 
     try {
-      const course = await courseService.findByIdWithEpisodes(id);
-      return res.json(course);
+      const course = await courseService.findByIdWithEpisodes(courseId);
+
+      if (!course) {
+        return res.status(404).json({ message: "Curso não encontrado" });
+      }
+
+      const liked = await likeService.isLiked(userId, +courseId);
+      const favorited = await favoriteService.isFavorited(userId, +courseId);
+      return res.json({ ...course.get(), liked, favorited });
     } catch (err) {
       if (error instanceof Error) {
         return res.status(400).json({ message: error.message });
--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -34,4 +34,12 @@ export const favoriteService = {
       },
     });
   },
+
+  isFavorited: async (userId: number, courseId: number) => {
+    const favorite = await Favorite.findOne({
+      where: { userId, courseId },
+    });
+
+    return favorite !== null;
+  },
 };
