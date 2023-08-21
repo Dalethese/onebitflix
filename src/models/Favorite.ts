81c5afd1c212a18e43ee6dc360486b0a774e358d
Jonas <jonasdalethese1@gmail.com>
2023-08-21 15:15:43 -0300

    adicionando favoritos (minha lista)

---
+++ src/models/Favorite.ts
@@ -0,0 +1,39 @@
+import { DataTypes, Model, Optional } from "sequelize";
+import { CourseInstance } from "./Course";
+import { UserInstance } from "./User";
+import { sequelize } from "../database";
+
+export interface Favorite {
+  userId: number;
+  courseId: number;
+}
+
+export interface FavoriteInstance extends Model<Favorite>, Favorite {
+  course?: CourseInstance;
+  user?: UserInstance;
+}
+
+export const Favorite = sequelize.define<FavoriteInstance, Favorite>("Favorite", {
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    references: {
+      model: "users",
+      key: "id",
+    },
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+  },
+  courseId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    references: {
+      model: "courses",
+      key: "id",
+    },
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+  },
+});
