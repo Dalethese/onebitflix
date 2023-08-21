bb2847004576222d39196ead6030f23568532072
Jonas <jonasdalethese1@gmail.com>
2023-08-21 15:37:07 -0300

    Obtendo os cursos favoritos

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -9,4 +9,20 @@ export const favoriteService = {
 
     return favorite;
   },
+
+  findByUserId: async (userId: number) => {
+    const favorites = await Favorite.findAll({
+      where: { userId },
+      attributes: [["user_id", "userId"]],
+      include: {
+        association: "Course",
+        attributes: ["id", "name", "synopsis", ["thumbnail_url", "thumbnailUrl"]],
+      },
+    });
+
+    return {
+      userId,
+      courses: favorites.map((favorite) => favorite.Course),
+    };
+  },
 };
